9dd0c831cfb9d0ba90afe466727043364c0978af
kb21-doofy <ujifc26kyouhei0201@ezweb.ne.jp>
2026-05-24 13:01:38 +0900

    add: home endpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    render json: { message: "ok" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "home" => "home#index"
+
   root "home#index"
 end
