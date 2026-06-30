1946e0b9deb238e0201af052b5061a8bf9771ee2
kb21-doofy <ujifc26kyouhei0201@ezweb.ne.jp>
2026-06-30 20:57:43 +0900

    execute: posts and user created to table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   validates :name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_24_000000) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_30_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "age", null: false
@@ -20,4 +29,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_24_000000) do
     t.string "name", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
